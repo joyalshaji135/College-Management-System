468d44fa81f09cbd2b8c754e11edd450787bb727
joyalshaji135 <joyalshaji135@gmail.com>
2024-05-30 22:56:00 +0530

    Subject is started

--- Admin-Side/add_course.php
+++ Admin-Side/add_course.php
@@ -41,16 +41,9 @@ include('./Borders/side_bar.php');
                                                 <div class="col-lg-6">
                                                 <select class="form-control m-bot15" name="programme_id" style="text-align: center;">
                                                     <?php
+                                            
                                                         Display_Programme();
-                                                        // $programme_select_query="Select * from programme_tbl";
-                                                        // $programme_select_main_query=mysqli_query($connection,$programme_select_query);
-                                                        // echo "<option selected disabled>-Select Programme-</option>";
-                                                        // while($programme_row=mysqli_fetch_assoc($programme_select_main_query))
-                                                        // {
-                                                        //     $programme_id=$programme_row['programme_id'];
-                                                        //     $programme_name=$programme_row['programme_name'];
-                                                        //     echo "<option value='$programme_id'>$programme_name</option>";
-                                                        // }
+                                            
                                                     ?>
                                                 </select>
                                                 </div>
